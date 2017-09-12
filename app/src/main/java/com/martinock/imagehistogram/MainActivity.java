68575d372e3280f4f7539a4b992e3e9623fcbe76
Martino Christanto <martino.aksel.11@gmail.com>
2017-09-12 16:28:58 +0700

    Add count object method

--- app/src/main/java/com/martinock/imagehistogram/MainActivity.java
+++ app/src/main/java/com/martinock/imagehistogram/MainActivity.java
@@ -1,10 +1,10 @@
 package com.martinock.imagehistogram;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,33 +14,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final int COLOR_BOUNDARIES = 256;
+    private static int BLACK_COLOR = Color.rgb(0, 0, 0);
+    private static int WHITE_COLOR = Color.rgb(255, 255, 255);
+
+    private int bwThreshold = 0;
+    private int objectCount = 0;
 
     private ImageView imageView;
-    private int[] redPixel = new int[COLOR_BOUNDARIES];
-    private int[] greenPixel = new int[COLOR_BOUNDARIES];
-    private int[] bluePixel = new int[COLOR_BOUNDARIES];
-    private int[] grayPixel = new int[COLOR_BOUNDARIES];
+    private Button convertButton;
+    private LinearLayout llSeekbar;
+    private SeekBar thresholdSeekbar;
+    private TextView tvSeekbar;
+    private Button countButton;
 
-    private BarChart redChart;
-    private BarChart greenChart;
-    private BarChart blueChart;
-    private BarChart grayChart;
+    private LinearLayout llObjectCount0;
+    private LinearLayout llObjectCount1;
+    private TextView tvObjectCount0;
+    private TextView tvObjectCount1;
+    private BitmapDrawable originalImageBitmap;
+    private Bitmap blackAndWhiteBitmap;
+
     private ProgressBar progressBar;
     private TextView tvTitle;
 
@@ -60,14 +66,160 @@ public class MainActivity extends AppCompatActivity
 
         imageView = (ImageView) findViewById(R.id.iv_photo);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        redChart = (BarChart) findViewById(R.id.red_chart);
-        greenChart = (BarChart) findViewById(R.id.green_chart);
-        blueChart = (BarChart) findViewById(R.id.blue_chart);
-        grayChart = (BarChart) findViewById(R.id.gray_chart);
+        convertButton = (Button) findViewById(R.id.btn_convert_to_bw);
+        thresholdSeekbar = (SeekBar) findViewById(R.id.seekbar_bw_threshold);
+        tvSeekbar = (TextView) findViewById(R.id.tv_bw_threshold);
+        llSeekbar = (LinearLayout) findViewById(R.id.ll_seekbar);
         tvTitle = (TextView) findViewById(R.id.first_title);
+        tvObjectCount0 = (TextView) findViewById(R.id.tv_object_count);
+        tvObjectCount1 = (TextView) findViewById(R.id.tv_object_count_1);
+        llObjectCount0 = (LinearLayout) findViewById(R.id.ll_object_count);
+        llObjectCount1 = (LinearLayout) findViewById(R.id.ll_object_count_1);
+        countButton = (Button) findViewById(R.id.btn_count_object);
+        setSeekbarListener();
+        setButtonListener();
         NavigationView navigationView = (NavigationView) findViewById(
                 R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setButtonListener() {
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLoading();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        convertToBW();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                hideLoading();
+                                countButton.setVisibility(View.VISIBLE);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+        countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLoading();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        countObject();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvObjectCount0.setText(
+                                        String.valueOf(objectCount));
+                                llObjectCount0.setVisibility(View.VISIBLE);
+                                hideLoading();
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+    }
+
+    private void countObject() {
+        int height = blackAndWhiteBitmap.getHeight();
+        int width = blackAndWhiteBitmap.getWidth();
+        final Bitmap copyOfBW = blackAndWhiteBitmap.copy(
+                Bitmap.Config.RGB_565, true);
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                int pixel = blackAndWhiteBitmap.getPixel(j, i);
+                int red = Color.red(pixel);
+                if (red == 0) {
+                    floodFill(j, i, BLACK_COLOR, WHITE_COLOR);
+                    objectCount++;
+                }
+            }
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                imageView.setImageBitmap(copyOfBW);
+            }
+        });
+    }
+
+    private void floodFill(int x, int y, int prevColor, int newColor) {
+        Queue<Integer> queueX = new LinkedList<Integer>();
+        Queue<Integer> queueY = new LinkedList<Integer>();
+        queueX.add(x);
+        queueY.add(y);
+        while (queueX.size() > 0) {
+            int pointX = queueX.poll();
+            int pointY = queueY.poll();
+            if (blackAndWhiteBitmap.getPixel(pointX, pointY) != prevColor) {
+                continue;
+            }
+
+            int nextX = pointX + 1;
+            while ((pointX > 0) && (blackAndWhiteBitmap.getPixel(pointX, pointY)
+                    == prevColor)) {
+                blackAndWhiteBitmap.setPixel(pointX, pointY, newColor);
+                if ((pointY > 0)
+                        && (blackAndWhiteBitmap.getPixel(pointX, pointY - 1)
+                        == newColor)) {
+                    queueX.add(pointX);
+                    queueY.add(pointY - 1);
+                }
+                if ((pointY < blackAndWhiteBitmap.getHeight() - 1)
+                        && (blackAndWhiteBitmap.getPixel(pointX, pointY + 1)
+                        == prevColor)) {
+                    queueX.add(pointX);
+                    queueY.add(pointY + 1);
+                }
+                pointX--;
+            }
+            while ((nextX < blackAndWhiteBitmap.getWidth() - 1)
+                    && (blackAndWhiteBitmap.getPixel(nextX, pointY)
+                    == prevColor)) {
+                blackAndWhiteBitmap.setPixel(nextX, pointY, newColor);
+
+                if ((pointY > 0) && (blackAndWhiteBitmap.getPixel(
+                        nextX, pointY - 1) == prevColor)) {
+                    queueX.add(nextX);
+                    queueY.add(pointY - 1);
+                }
+                if ((pointY < blackAndWhiteBitmap.getHeight() - 1)
+                        && (blackAndWhiteBitmap.getPixel(nextX, pointY + 1)
+                        == prevColor)) {
+                    queueX.add(nextX);
+                    queueY.add(pointY + 1);
+                }
+                nextX++;
+            }
+        }
+    }
+
+    private void setSeekbarListener() {
+        thresholdSeekbar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(
+                    SeekBar seekBar, int progress, boolean fromUser) {
+                bwThreshold = progress;
+                tvSeekbar.setText(String.valueOf(bwThreshold));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
@@ -86,46 +238,16 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.photo_1) {
-            changeImage(R.drawable.photo_1);
-        } else if (id == R.id.photo_2) {
-            changeImage(R.drawable.photo_2);
-        } else if (id == R.id.photo_3) {
-            changeImage(R.drawable.photo_3);
-        } else if (id == R.id.photo_4) {
-            changeImage(R.drawable.photo_4);
-        } else if (id == R.id.photo_5) {
-            changeImage(R.drawable.photo_5);
-        } else if (id == R.id.photo_6) {
-            changeImage(R.drawable.photo_6);
-        } else if (id == R.id.photo_7) {
-            changeImage(R.drawable.photo_7);
-        } else if (id == R.id.photo_8) {
-            changeImage(R.drawable.photo_8);
-        } else if (id == R.id.photo_9) {
-            changeImage(R.drawable.photo_9);
-        } else if (id == R.id.photo_10) {
-            changeImage(R.drawable.photo_10);
-        } else if (id == R.id.photo_11) {
-            changeImage(R.drawable.photo_11);
+            changeImage(R.drawable.photo_date);
         } else {
-            changeImage(R.drawable.photo_12);
+            changeImage(R.drawable.photo_1);
         }
+        tvTitle.setVisibility(View.GONE);
+        objectCount = 0;
+        showAllView();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        showLoading();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                analyzeBitmap();
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        hideLoading();
-                    }
-                });
-            }
-        }).start();
         return true;
     }
 
@@ -141,83 +263,50 @@ public class MainActivity extends AppCompatActivity
 
     private void hideAllView() {
         imageView.setVisibility(View.GONE);
-        redChart.setVisibility(View.GONE);
-        greenChart.setVisibility(View.GONE);
-        blueChart.setVisibility(View.GONE);
-        grayChart.setVisibility(View.GONE);
         tvTitle.setVisibility(View.GONE);
+        convertButton.setVisibility(View.GONE);
+        llSeekbar.setVisibility(View.GONE);
+        llObjectCount0.setVisibility(View.GONE);
+        llObjectCount1.setVisibility(View.GONE);
+        countButton.setVisibility(View.GONE);
     }
 
     private void showAllView() {
         imageView.setVisibility(View.VISIBLE);
-        redChart.setVisibility(View.VISIBLE);
-        greenChart.setVisibility(View.VISIBLE);
-        blueChart.setVisibility(View.VISIBLE);
-        grayChart.setVisibility(View.VISIBLE);
+        convertButton.setVisibility(View.VISIBLE);
+        llSeekbar.setVisibility(View.VISIBLE);
+        convertButton.setVisibility(View.VISIBLE);
+        thresholdSeekbar.setVisibility(View.VISIBLE);
+        tvSeekbar.setVisibility(View.VISIBLE);
     }
 
     private void changeImage(int newImageId) {
         Drawable newImage = ResourcesCompat.getDrawable(
                 getResources(), newImageId, null);
         imageView.setImageDrawable(newImage);
+        originalImageBitmap = (BitmapDrawable) imageView.getDrawable();
     }
 
-    private void analyzeBitmap() {
-        BitmapDrawable bd = (BitmapDrawable) imageView.getDrawable();
-        int height = bd.getBitmap().getHeight();
-        int width = bd.getBitmap().getWidth();
+    private void convertToBW() {
+        int height = originalImageBitmap.getBitmap().getHeight();
+        int width = originalImageBitmap.getBitmap().getWidth();
+        blackAndWhiteBitmap = Bitmap.createBitmap(
+                width, height, Bitmap.Config.RGB_565);
         for (int i = 0; i < height; ++i) {
             for (int j = 0; j < width; ++j) {
-                int pixel = bd.getBitmap().getPixel(j, i);
+                int pixel = originalImageBitmap.getBitmap().getPixel(j, i);
                 int red = Color.red(pixel);
                 int green = Color.green(pixel);
                 int blue = Color.blue(pixel);
                 int gray = ((red + green + blue) / 3) % 256;
-                redPixel[red]++;
-                greenPixel[green]++;
-                bluePixel[blue]++;
-                grayPixel[gray]++;
+                if (gray < bwThreshold) {
+                    blackAndWhiteBitmap.setPixel(j, i, BLACK_COLOR);
+                } else {
+                    blackAndWhiteBitmap.setPixel(j, i, WHITE_COLOR);
+                }
             }
         }
-        visualizeHistogram();
-    }
 
-    private void visualizeHistogram() {
-        List<BarEntry> redEntries = new ArrayList<>();
-        List<BarEntry> greenEntries = new ArrayList<>();
-        List<BarEntry> blueEntries = new ArrayList<>();
-        List<BarEntry> grayEntries = new ArrayList<>();
-
-        for (int i = 0; i < COLOR_BOUNDARIES; ++i) {
-            redEntries.add(new BarEntry(i, redPixel[i]));
-            greenEntries.add(new BarEntry(i, greenPixel[i]));
-            blueEntries.add(new BarEntry(i, bluePixel[i]));
-            grayEntries.add(new BarEntry(i, grayPixel[i]));
-        }
-
-        BarDataSet redDataSet = new BarDataSet(redEntries, "Red");
-        BarDataSet greenDataSet = new BarDataSet(greenEntries, "Green");
-        BarDataSet blueDataSet = new BarDataSet(blueEntries, "Blue");
-        BarDataSet grayDataSet = new BarDataSet(grayEntries, "Gray");
-
-        redDataSet.setColor(Color.RED);
-        greenDataSet.setColor(Color.GREEN);
-        blueDataSet.setColor(Color.BLUE);
-        grayDataSet.setColor(Color.GRAY);
-
-        BarData redData = new BarData(redDataSet);
-        BarData greenData = new BarData(greenDataSet);
-        BarData blueData = new BarData(blueDataSet);
-        BarData grayData = new BarData(grayDataSet);
-
-        redChart.setData(redData);
-        greenChart.setData(greenData);
-        blueChart.setData(blueData);
-        grayChart.setData(grayData);
-
-        redChart.invalidate();
-        greenChart.invalidate();
-        blueChart.invalidate();
-        grayChart.invalidate();
+        imageView.setImageBitmap(blackAndWhiteBitmap);
     }
 }
